08162f6956f8acb99b6007c679e0b0df3897b13d
Niklas Schmitt <ns@znuny.com>
2023-01-06 11:29:33 +0100

    Applied ZnunyCodePolicy.

--- var/httpd/htdocs/js/GoogleMapsMarkerClusterer.js
+++ var/httpd/htdocs/js/GoogleMapsMarkerClusterer.js
@@ -1,6 +1,6 @@
 // --
 // Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
-// Copyright (C) 2012-2022 Znuny GmbH, https://znuny.com/
+// Copyright (C) 2012 Znuny GmbH, https://znuny.com/
 // --
 // $origin: znuny - 460ef44565300c6b979b0743833e3800fdbebf81 - var/httpd/htdocs/js/Core.Agent.Search.js
 // --
